d49edd92e22903b76faddbe9f22e29ec0028b1d0
iman <git@github.com:imaneng/my-prject.git>
2021-11-25 18:51:50 +0330

    new rel

---
+++ test2.py
@@ -0,0 +1,12 @@
+adad = input("adad khod ra vared kondi: ")
+for i in adad :
+    print(i , ":", i*int(i))
+    print('\n')
+    # t =int(i)
+    # while t :
+    #     t= t -1
+    #     print(i, end='\t')
+        
+
+
+
